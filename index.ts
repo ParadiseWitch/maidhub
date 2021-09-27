554e20d20a229096ee7efd8c2f1891eb92c03137
Maiiiiiid <zhp92@outlook.com>
2021-09-27 11:15:19 +0800

    refactor(nest): :recycle: Interfacing with the Nest.js framework

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { HandleDom } from "./collectdata";
-
-(async () => {
-  const handleDom = await new HandleDom("http://www.ruanyifeng.com/blog/").init();
-  const data = handleDom.getDomAttr("#homepage .module-list-item a","href");
-  console.log(data);
-})()
-
