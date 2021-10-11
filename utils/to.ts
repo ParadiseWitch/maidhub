e6bc3a2a57e39086799fe56c565462942dac55a8
Maiiiiiid <zhp92@outlook.com>
2021-10-11 17:25:47 +0800

    feat: :seedling: add to.ts utility class to help `await` handle exception

---
+++ utils/to.ts
@@ -0,0 +1,15 @@
+export default function to(promise) {
+  if (!promise || !Promise.prototype.isPrototypeOf(promise)) {
+    return new Promise((resolve, reject) => {
+      reject(new Error("requires promises as the param"));
+    }).catch((err) => {
+      return [err, null];
+    });
+  }
+  // TODO 约束返回类型
+  return promise.then(function () {
+    return [null, ...arguments];
+  }).catch(err => {
+    return [err, null];
+  });
+};
