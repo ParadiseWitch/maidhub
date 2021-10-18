ab88102a6c64f2bde9757896a7306dbe3a74ab68
Maiiiiiid <zhp92@outlook.com>
2021-10-18 12:29:25 +0800

    refactor(util): :recycle: refactor log util

--- utils/log.ts
+++ utils/log.ts
@@ -1,14 +1,33 @@
 const fs = require('fs')
-const logPath = '/path/'
-const logName = 'log.txt'
+import { writeFileSync } from 'fs'
+import { dateFormat } from './DateUtil'
 
-export const log = (content: any) => {
-  fs.writeFile(logPath + logName, content, err => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    //文件写入成功。
-  })  
+export default class Log {
+  private static logPath = './log/';
+  private static logName = 'log.txt';
+  public static log(...contents: string[]) {
+    Log.writeLogFile("info", ...contents);
+  }
+  public static debug(...contents: string[]) {
+    Log.writeLogFile("debug", ...contents);
+  }
+  public static error(...contents: string[]) {
+    Log.writeLogFile("error", ...contents);
+  }
+  public static warn(...contents: string[]) {
+    Log.writeLogFile("warn", ...contents);
+  }
+  public static info(...contents: string[]) {
+    Log.writeLogFile("warn");
+  }
+
+  public static writeLogFile(level: string, ...contents: string[]) {
+    const logFilePath = this.logPath + this.logName;
+    const now = dateFormat(new Date());
+    const newLine = `[${level}]\t${now}\t${contents.join(", ")}\n`;
+    const data = fs.readFileSync(logFilePath);
+    fs.writeFileSync(logFilePath, data + newLine)
+  }
 }
+
 
